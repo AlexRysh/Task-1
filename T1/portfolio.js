2185bfb47fe46f3feda17a6f8c298798c6a16d47
Oleksandr Ryzhanovskyi <Oleksandr_Ryzhanovskyi@epam.com>
2018-08-20 18:31:17 +0300

    task 1 added

---
+++ T1/portfolio.js
@@ -0,0 +1,32 @@
+// init Isotope
+var iso = new Isotope( '.portfolio__item-board', {
+    itemSelector: '.item-board__item',
+    layoutMode: 'fitRows'
+});
+
+// bind filter button click
+var filtersElem = document.querySelector('.portfolio__filter');
+filtersElem.addEventListener( 'click', function( event ) {
+    if ( !matchesSelector( event.target, 'filter__button' ) ) {
+        return;
+    }
+    var filterValue = event.target.getAttribute('data-filter');
+    iso.arrange({ filter: filterValue });
+});
+
+// change filter__button_checked class on buttons
+var buttonGroups = document.querySelectorAll('.portfolio__filter');
+for ( var i=0, len = buttonGroups.length; i < len; i++ ) {
+    var buttonGroup = buttonGroups[i];
+    radioButtonGroup( buttonGroup );
+}
+
+function radioButtonGroup( buttonGroup ) {
+    buttonGroup.addEventListener( 'click', function( event ) {
+        if ( !matchesSelector( event.target, 'filter__button' ) ) {
+            return;
+        }
+        buttonGroup.querySelector('.filter__button_checked').classList.remove('filter__button_checked');
+        event.target.classList.add('filter__button_checked');
+    });
+}
